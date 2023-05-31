8df07d2c274b8c62fd00f632a704aa6ef67e16ac
renaldi24ahmad <syahrenaldinur@gmail.com>
2023-05-31 12:40:25 +0800

    selesai pertemuan 3

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <span>&copy; <?php echo date('Y'); ?> - FTI UNISKA</span>
+    </div>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
